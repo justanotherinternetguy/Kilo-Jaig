82b6441734f62048107e62eb0226d686c9efb73e
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-12 22:52:41 -0400

    e

--- kilo.c
+++ kilo.c
@@ -1,19 +1,16 @@
-#include <unistd.h>
 #include <termios.h>
-
+#include <unistd.h>
 
 void enableRawMode() {
   struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-
+  tcgetattr(STDIN_FILENO, & raw);
   raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, & raw);
 }
 
 int main() {
   enableRawMode();
-
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, & c, 1) == 1 && c != 'q');
   return 0;
 }
